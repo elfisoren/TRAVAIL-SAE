0c4df557abd001ebc55d460427a2983cc33681c8
elfisoren <152414566+elfisoren@users.noreply.github.com>
2023-12-19 16:59:43 +0100

    save 19 décembre

---
+++ SAE/test_tools/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tools.h"
+int main()
+{
+}
---
+++ SAE/tools.c/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tools.h"
+
+#define LIGNES 2
+#define COLONNES 6
+int main()
+{
+    executerJeu();
+    afficherMenu();
+    jeuDesPaires();
+    initialiserCartes();
+    melangerCartes();
+    afficherCarte();
+}
